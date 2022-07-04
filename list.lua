e70eb0d7b136ff9a358428bc9ed16d6ac5ab80d5
RealSiesgo <93088981+RealSiesgo@users.noreply.github.com>
2022-07-04 08:45:38 -0300

    Update list.lua

--- list.lua
+++ list.lua
@@ -1,5 +1,5 @@
 function isblacklisted(hwid)
-	local hwidhashes={"9055c87fc449d8e268e9a5a075eb0fad2a97bfca"}
+	local hwidhashes={}--{"9055c87fc449d8e268e9a5a075eb0fad2a97bfca"}
 	for i,hash in next,hwidhashes do
 		if hwid:match("^"..hash) then
 			return true
